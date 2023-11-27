b2ee0c07e41ad2c65b455e7662b6519b7dc9b667
nandersen1 <112381474+nandersen1@users.noreply.github.com>
2023-11-27 15:33:18 +0000

    update app.js with better GUI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,69 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  // Zustände für die Eingabe und Ergebnisse der Bildanalyse bzw. -generierung
+  const [imageUrl, setImageUrl] = useState('');
+  const [analysisResult, setAnalysisResult] = useState('');
+  const [generationPrompt, setGenerationPrompt] = useState('');
+
+  // Funktion für die Bildanalyse
+  const handleImageAnalysis = () => {
+    // Hier könnten Sie den Code für die Bildanalyse einfügen
+    // Verwenden Sie imageUrl für den Zugriff auf die eingegebene URL
+    // Setzen Sie das Ergebnis mit setAnalysisResult
+    // Beispiel: analyzeImage(imageUrl).then(result => setAnalysisResult(result));
+  };
+
+  // Funktion für die Bildgenerierung
+  const handleImageGeneration = () => {
+    // Hier könnten Sie den Code für die Bildgenerierung einfügen
+    // Verwenden Sie generationPrompt für den Zugriff auf die eingegebene Aufforderung
+    // Setzen Sie das generierte Bild mit setGeneratedImage
+    // Beispiel: generateImage(generationPrompt).then(image => setGeneratedImage(image));
+  };
+
+  return (
+    <div>
+      <h1>Image Analysis and Generation App</h1>
+
+      {/* Eingabefeld für die Bildanalyse */}
+      <label>
+        Image URL:
+        <input
+          type="text"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+      </label>
+
+      {/* Schaltfläche für die Bildanalyse */}
+      <button onClick={handleImageAnalysis}>Analyze Image</button>
+
+      {/* Ergebnis der Bildanalyse anzeigen */}
+      {analysisResult && (
+        <div>
+          <h2>Analysis Result:</h2>
+          <p>{analysisResult}</p>
+        </div>
+      )}
+
+      {/* Eingabefeld für die Bildgenerierung */}
+      <label>
+        Generation Prompt:
+        <input
+          type="text"
+          value={generationPrompt}
+          onChange={(e) => setGenerationPrompt(e.target.value)}
+        />
+      </label>
+
+      {/* Schaltfläche für die Bildgenerierung */}
+      <button onClick={handleImageGeneration}>Generate Image</button>
+
+      {/* Hier können Sie das generierte Bild oder den Status der Generierung anzeigen */}
+      {/* Beispiel: {generatedImage && <img src={generatedImage} alt="Generated" />} */}
+    </div>
+  );
 }
 
 export default App;
