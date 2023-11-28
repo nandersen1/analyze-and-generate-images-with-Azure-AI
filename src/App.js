60a4db69fb4e1fdc878f1627ce4891bf344f4d34
nandersen1 <112381474+nandersen1@users.noreply.github.com>
2023-11-28 09:19:40 +0000

    bug/fix-runerror

--- src/App.js
+++ src/App.js
@@ -1,67 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  // Zustände für die Eingabe und Ergebnisse der Bildanalyse bzw. -generierung
-  const [imageUrl, setImageUrl] = useState('');
-  const [analysisResult, setAnalysisResult] = useState('');
-  const [generationPrompt, setGenerationPrompt] = useState('');
+  const [inputValue, setInputValue] = useState('');
 
-  // Funktion für die Bildanalyse
-  const handleImageAnalysis = () => {
-    // Hier könnten Sie den Code für die Bildanalyse einfügen
-    // Verwenden Sie imageUrl für den Zugriff auf die eingegebene URL
-    // Setzen Sie das Ergebnis mit setAnalysisResult
-    // Beispiel: analyzeImage(imageUrl).then(result => setAnalysisResult(result));
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
   };
 
-  // Funktion für die Bildgenerierung
-  const handleImageGeneration = () => {
-    // Hier könnten Sie den Code für die Bildgenerierung einfügen
-    // Verwenden Sie generationPrompt für den Zugriff auf die eingegebene Aufforderung
-    // Setzen Sie das generierte Bild mit setGeneratedImage
-    // Beispiel: generateImage(generationPrompt).then(image => setGeneratedImage(image));
+  const handleButtonAction = (action) => {
+    // Hier können Sie den Code für die gewählte Aktion (Bildanalyse/Bildgenerierung) hinzufügen
+    // Verwenden Sie dazu den Wert in der 'inputValue'-State-Variable
+    console.log(`Insert URL or type prompt '${action}' auslösen: ${inputValue}`);
   };
 
   return (
     <div>
-      <h1>Image Analysis and Generation App</h1>
-
-      {/* Eingabefeld für die Bildanalyse */}
+      <h1>Computer vision</h1>
       <label>
-        Image URL:
-        <input
-          type="text"
-          value={imageUrl}
-          onChange={(e) => setImageUrl(e.target.value)}
-        />
+      <div>Insert URL or type prompt:</div>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
       </label>
-
-      {/* Schaltfläche für die Bildanalyse */}
-      <button onClick={handleImageAnalysis}>Analyze Image</button>
-
-      {/* Ergebnis der Bildanalyse anzeigen */}
-      {analysisResult && (
-        <div>
-          <h2>Analysis Result:</h2>
-          <p>{analysisResult}</p>
-        </div>
-      )}
-
-      {/* Eingabefeld für die Bildgenerierung */}
-      <label>
-        Generation Prompt:
-        <input
-          type="text"
-          value={generationPrompt}
-          onChange={(e) => setGenerationPrompt(e.target.value)}
-        />
-      </label>
-
-      {/* Schaltfläche für die Bildgenerierung */}
-      <button onClick={handleImageGeneration}>Generate Image</button>
-
-      {/* Hier können Sie das generierte Bild oder den Status der Generierung anzeigen */}
-      {/* Beispiel: {generatedImage && <img src={generatedImage} alt="Generated" />} */}
+      <br />
+      <button onClick={() => handleButtonAction('Bildanalyse')}>Analyze</button>
+      <button onClick={() => handleButtonAction('Bildgenerierung')}>Generate</button>
     </div>
   );
 }
